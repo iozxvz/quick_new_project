92cef6b84db6fda85c53489b0e8014b5162b829c
fangchao <w_fangchao@xiwang.com>
2023-06-06 18:29:24 +0800

    swift ui support

---
+++ TEMPLATE_IOS_SWIFTUI/TEMPLATE_IOS_SWIFTUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TEMPLATE_IOS_SWIFTUI
+//
+//  Created by 方超 on 2023/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TEMPLATE_IOS_SWIFTUI/TEMPLATE_IOS_SWIFTUI/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEMPLATE_IOS_SWIFTUIApp.swift
+//  TEMPLATE_IOS_SWIFTUI
+//
+//  Created by 方超 on 2023/6/6.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
