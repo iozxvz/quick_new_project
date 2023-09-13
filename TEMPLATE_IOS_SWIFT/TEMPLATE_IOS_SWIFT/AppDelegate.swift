d69f29e9141974547d8c37635cd309aaf860c8c9
fangchao <w_fangchao@xiwang.com>
2023-09-13 11:08:26 +0800

    app start fix

--- TEMPLATE_IOS_SWIFT/TEMPLATE_IOS_SWIFT/AppDelegate.swift
+++ TEMPLATE_IOS_SWIFT/TEMPLATE_IOS_SWIFT/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
         window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
         return true
     }
 }
